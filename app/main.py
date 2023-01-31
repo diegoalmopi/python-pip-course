33d603fe61b961047a64e107330f8754fd451a45
Diego Moreno <diegomoreno062@gmail.com>
2023-01-31 17:54:44 -0500

    cambios finales

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
-def run():  
+def run(): 
+
   data = read_csv.read_csv('data.csv')
   country = input('Digite el país ==> ')
   print(country)
@@ -16,13 +18,20 @@ def run():
    charts.generate_bar_chart(country,labels,values)
 
     #Filtrado de % poblacion en sur America
+  '''
   data = list(filter(lambda item:item['Continent']=='South America',data))
   labels = [dict['Country/Territory'] for dict in data]
   values = [dict['World Population Percentage'] for dict in data]
-
+  '''
   #labels = list(map(lambda item: item['Country/Territory'],data))
   #values = list(map(lambda item: item['World Population Percentage'],data))
-  charts.generate_pie_chart(labels,values)
+  
+  #Using Pandas
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent']=='Africa']
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries,percentages)
   
   
   
